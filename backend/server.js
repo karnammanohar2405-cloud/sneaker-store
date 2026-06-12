1b12a5cf583fad711aa716db498289d3d030117b
manohar <karnammanohar2405@gmail.com>
2026-06-13 01:15:45 +0530

    lulu

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ connectDB();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://sneaker-store-1-5qub.onrender.com"
+    ],
     credentials: true,
   })
 );
